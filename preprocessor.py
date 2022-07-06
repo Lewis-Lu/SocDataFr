8643c026c1fb5caa38f6f05a5852c7b726f5d0a3
lewis <luh.lewis@gmail.com>
2022-07-07 00:37:32 +0800

    update #20220707 v0.1 add test unit

--- preprocessor.py
+++ preprocessor.py
@@ -24,13 +24,25 @@ class preprocessor(object):
 
         self._stopwordsfiles = ["./stopwords/cn_stopwords.txt", "./stopwords/hit_stopwords.txt"]
         self._stopwords = self.init_stopwords()
-        self._dataframe = pds.DataFrame(self._file)
+        self._dataframe = pds.DataFrame(self._data)
 
-    def print_DataFrameHeader(self):
+    def return_DataFrame_column(self):
+        return self._dataframe.columns
+
+    def print_DataFrame_Header(self):
         if self._dataframe is not None:
             print( self._dataframe.head() )
         else:
             raise Warning("data frame not existed.")
+
+    def print_DataFrame_Summary(self):
+        print( f"This function list the data frame summary of the {self._file}, including the line numbers and #None in each indexed column" )
+        print( self.return_DataFrame_column() )
+        print( f"the shape of the data frame is (#rows, #cols): ({self._dataframe.index} , {self._dataframe.columns})" )
+        
+
+    def remove_None_row(self):
+        pass
 
     def init_stopwords(self):
         rtn = []
@@ -40,12 +52,12 @@ class preprocessor(object):
             rtn.append(tmp)
         return rtn
 
-    def format_coloumn_as_pure_chinese(self, coloumn_text):
+    def format_column_as_pure_chinese(self, column_text):
         '''Purge Non-Chinese Characters'''
-        if coloumn_text not in self._dataframe.columns:
-            raise ValueError(r"DataFrame does not have colomun named as ${coloumn_text}")
+        if column_text not in self._dataframe.columns:
+            raise ValueError(f"DataFrame does not have colomun named as {column_text}")
         else:
-            document_list = list(self._dataframe[coloumn_text])
+            document_list = list(self._dataframe[column_text])
             chinese_list = []
             for doc in document_list:
                 if type(doc) is not str:
@@ -59,13 +71,13 @@ class preprocessor(object):
                 chinese_list.append(content_str)
             return chinese_list
 
-    def split_jieba(self, split_coloumn_name):
+    def split_jieba(self, split_column_name):
         ''' 
-        split the designated coloumn in the dataframe 
+        split the designated column in the dataframe 
         
         @return: return the cleaned (w/o stopwords) splitted word list
         '''
-        chinese_list = self.format_coloumn_as_pure_chinese(split_coloumn_name)
+        chinese_list = self.format_column_as_pure_chinese(split_column_name)
         split_word_list = []
         for clist in chinese_list:
             res = []    
@@ -105,16 +117,16 @@ class preprocessor(object):
         # 输出不在停用词库中的高频词
         print("\n".join(str(i) for i in need_to_add_stopword))  
 
-    def cleaned_to_new_dataframe_coloumn(self, split_coloumn_name, new_coloumn_name):
+    def cleaned_to_new_dataframe_column(self, split_column_name, new_column_name):
         word_list = []
-        cleaned_contents = self.split_jieba(split_coloumn_name)
+        cleaned_contents = self.split_jieba(split_column_name)
         for i in range(len( cleaned_contents )):
             k_list=['']
             for j in range(len( cleaned_contents[i] )):
                 k_list[0] += (cleaned_contents[i][j] + ' ')
             word_list.append(k_list[0])
     
-        df_new_col = pds.DataFrame({new_coloumn_name:word_list})
+        df_new_col = pds.DataFrame({new_column_name:word_list})
         print( df_new_col.head() )
         
         self._old_dataframe = self._dataframe
@@ -122,13 +134,27 @@ class preprocessor(object):
 
     def save_as_new_csv(self, new_csv_filename):
         if self._old_dataframe is not None:
-            raise ValueError("No new coloumn of the Data Frame is designated.")
+            raise ValueError("No new column of the Data Frame is designated.")
         else:
             self._dataframe.to_csv(new_csv_filename, index=True, encoding=self._encoding)
 
 
+# test unit for preprocessor
 def main():
-    pass
+    filename = "./data/data_motion.csv"
+    proc = preprocessor(filename)
+
+    # # print the data frame header to valid 
+    # proc.print_DataFrame_Header()
+
+    proc.print_DataFrame_Summary()
+
+    indexed_cols = proc.return_DataFrame_column() 
+    # print the column indexed strings
+    # print( indexed_cols )
+    
+    cleaned_chinese_list = proc.format_column_as_pure_chinese(indexed_cols[1])
+    # print( len(cleaned_chinese_list) )
 
 if __name__ == '__main__':
     main()
